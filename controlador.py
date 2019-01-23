b685ede34ecf572efea5314d8aad2ca0e9dbe978
Jorge Dominguez <domorqfut1998@gmail.com>
2019-01-22 21:15:28 -0500

    crecacion de cuadro de dialogo en caso de no meter informacion adecuada.

--- controlador.py
+++ controlador.py
@@ -1,1 +1,8 @@
-print("controlador en proceso de creación")
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Dialogo(QtWidgets.QDialog):
+    def init(self):
+        QtWidgets.QDialog.init(self)
+        self.resize(290, 290)
+        self.setWindowTitle("¡¡ALERTA!!")
+        self.etiqueta = QtWidgets.QLabel(self)
