7ede3562987c8cb202a001824ac487a77aa71f93
Marco Sanabria <msanabria@est.ups.edu.ec>
2019-02-05 20:10:21 -0500

    controller_final

--- controller.py
+++ controller.py
@@ -2,28 +2,34 @@ import model
 
 
 class control:
-    def __init__(self, peso, altura, path):
+    def __init__(self, peso, altura, nombre,bol):
         self.peso = float(peso)
         self.altura = float(altura)
-        self.path = path
+        self.path = r"C:\Users\Pc\Downloads"
+        self.nombre= nombre
         self.estado = ""
+        self.bol= bol
         self.a = model.Archivo()
-        self.defineEstado()
+
 
     def defineEstado(self):
         e = self.peso / ((self.altura / 100) ** 2)
         print(e)
-        if e < 15.00:
-            self.a.delgadesSevera(self.path)
-        if e >= 15.00 and e < 19.00:
-            self.a.delgadesModerada(self.path)
-        if e >= 19.00 and e < 25.00:
-            self.a.pesoNormal(self.path)
-        if e >= 25.00 and e < 29.00:
-            self.a.sobrepeso(self.path)
-        if e >= 29.00:
-            self.a.obesidad(self.path)
-
-
-c = control(20, 180, "D:")
-print(c.estado)
+        s=""
+        if e < 16.00:
+            s = self.a.delgadesSevera(self.path,self.nombre,self.bol)
+            return s
+        if e >= 16.00 and e < 18.49:
+            s = self.a.delgadesModerada(self.path,self.nombre,self.bol)
+            return s
+        if e >= 18.50 and e < 25.00:
+            s = self.a.pesoNormal(self.path,self.nombre,self.bol)
+            return s
+        if e >= 25.00 and e < 35.00:
+            s = self.a.sobrepeso(self.path,self.nombre,self.bol)
+            return s
+        if e >= 35:
+            print("ken--------------")
+            s = self.a.obesidad(self.path,self.nombre,self.bol)
+            return s
+        return s
